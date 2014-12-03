38bea8d1caf32e8a979c814b5f47c2b46068faf1
Lord_Ralex <lordralex@gmail.com>
2014-12-03 11:59:21 -0600

    Revert "Minor touchup"
    This reverts commit 7f08abe4e18974ade6e48290701b93d9fbb9fddd.

--- assets/search.js
+++ assets/search.js
@@ -19,12 +19,11 @@ function parseData(data) {
     }
     viewModel.tableLoaded(true);
 }
-function searchKeyPress(e) {
-    if (typeof e == 'undefined' && window.event) {
-        e = window.event;
-    }
-    if (e.keyCode == 13)
+function searchKeyPress(e)
     {
-        document.getElementById('searchButton').click();
+        if (typeof e == 'undefined' && window.event) { e = window.event; }
+        if (e.keyCode == 13)
+        {
+            document.getElementById('searchButton').click();
+        }
     }
-}
